7c783a21e2973798a66f0affc863bebaa3c72bd2
Stébou Mendy <stebou8mendy@gmail.com>
2024-04-28 15:42:07 +0200

    Modify controller and add form to create product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use App\Form\ProductType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductController extends AbstractController
 {
@@ -38,6 +42,28 @@ class ProductController extends AbstractController
 
         return $this->render('product/show.html.twig', [
             'product' => $product,
+        ]);
+    }
+
+    #[Route('/admin/product/create', name: 'product_create')]
+    public function create(Request $request, SluggerInterface $slugger, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(ProductType::class);
+            
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $product = $form->getData();
+            $product->setSlug(strtolower($slugger->slug($product->getName())));
+            $em->persist($product);
+            $em->flush();
+            dd($product);
+        }
+
+        $formView = $form->createView();
+
+        return $this->render('product/create.html.twig', [
+            'formView' => $formView
         ]);
     }
 }
