636622ac0a97acd8b2dfec4aa15d918c5e1c4b86
Stébou Mendy <stebou8mendy@gmail.com>
2024-05-19 14:58:14 +0200

    Doctrine Listeners

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -17,9 +17,11 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
-    
-    public function __construct(readonly SluggerInterface $slugger, readonly UserPasswordHasherInterface $encoder ) {}
-    
+
+    public function __construct(readonly SluggerInterface $slugger, readonly UserPasswordHasherInterface $encoder)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
@@ -32,18 +34,16 @@ class AppFixtures extends Fixture
             ->setEmail("admin@gmail.com")
             ->setFullName("Admin")
             ->setPassword($hash)
-            ->setRoles(['ROLE_ADMIN'])
-        ;
+            ->setRoles(['ROLE_ADMIN']);
         $manager->persist($admin);
 
-        for ($u=0; $u < 5; $u++) { 
+        for ($u = 0; $u < 5; $u++) {
             $user = new User();
             $hash = $this->encoder->hashPassword($user, "password");
             $user
                 ->setEmail("user$u@gmail.com")
                 ->setFullName($faker->name())
-                ->setPassword($hash)
-            ;
+                ->setPassword($hash);
             $users[] = $user;
             $manager->persist($user);
         }
@@ -51,34 +51,33 @@ class AppFixtures extends Fixture
         $users[] = $admin;
         $products = [];
 
-        for ($c=0; $c < 3; $c++) { 
+        for ($c = 0; $c < 3; $c++) {
             $category = new Category();
 
             $category
                 ->setName($faker->words(mt_rand(2, 4), true))
                 ->setSlug(strtolower($this->slugger->slug($category->getName())))
-                ->setOwner($faker->randomElement($users))
-            ;
+                ->setOwner($faker->randomElement($users));
 
             $manager->persist($category);
 
-            for ($p=0; $p < mt_rand(15, 20); $p++) { 
+            for ($p = 0; $p < mt_rand(15, 20); $p++) {
                 $product = new Product();
                 $product
                     ->setName($faker->words(mt_rand(2, 4), true))
                     ->setPrice(mt_rand(4000, 20000))
-                    ->setSlug(strtolower($this->slugger->slug($product->getName())))
+                    // ->setSlug(strtolower($this->slugger->slug($product->getName())))
                     ->setCategory($category)
                     ->setShortDescription($faker->paragraph())
                     ->setMainPicture('https://picsum.photos/400/400?image=' . mt_rand(100, 700));
 
                 $products[] = $product;
-                
+
                 $manager->persist($product);
             }
         }
 
-        for ($p=0; $p < mt_rand(20, 40); $p++) { 
+        for ($p = 0; $p < mt_rand(20, 40); $p++) {
             $purchase = new Purchase();
             $purchase
                 ->setFullName($faker->words(mt_rand(2, 4), true))
@@ -87,8 +86,7 @@ class AppFixtures extends Fixture
                 ->setCity($faker->city)
                 ->setUser($faker->randomElement($users))
                 ->setTotal(mt_rand(2000, 30000))
-                ->setPurchaseAt(DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-6months')))
-                ;
+                ->setPurchaseAt(DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-6months')));
 
             $selectProducts = $faker->randomElements($products, mt_rand(3, 5));
 
@@ -96,18 +94,17 @@ class AppFixtures extends Fixture
                 $purchaseItem = new PurchaseItem();
                 $purchaseItem
                     ->setProduct($product)
-                    ->setQuantity(mt_rand(3,5))
+                    ->setQuantity(mt_rand(3, 5))
                     ->setProductName($product->getName())
                     ->setProductPrice($product->getPrice())
                     ->setTotal(
                         $purchaseItem->getProductPrice() * $purchaseItem->getQuantity()
                     )
-                    ->setPurchase($purchase)
-                    ;
+                    ->setPurchase($purchase);
                 $manager->persist($purchaseItem);
             }
-            
-            if($faker->boolean(90)) {
+
+            if ($faker->boolean(90)) {
                 $purchase->setStatus(Purchase::STATUS_PAID);
             }
             $manager->persist($purchase);
